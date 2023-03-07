a07bd440389932729102f3c289072e8271cb3165
Moresa-art <115161130+Moresa-art@users.noreply.github.com>
2023-03-07 16:25:31 +0000

    sa

--- Assets/Scripts/MultiplePrefabs.cs
+++ Assets/Scripts/MultiplePrefabs.cs
@@ -1,18 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit.AR;
 
 public class MultiplePrefabs : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+{ 
+public List<GameObject> Prefabs;
+    private ARPlacementInteractable interactable;
+private int prefabIndex;
 
-    // Update is called once per frame
-    void Update()
+    private void Awake()
     {
+        SetPrefabIndex();
+        interactable = GetComponent<ARPlacementInteractable>();
+        interactable.placementPrefab = Prefabs[prefabIndex];
+    }
+    private void SetPrefabIndex()
+    {
+    prefabIndex = Random.Range(0, Prefabs.Count);
+    }
+   
+    public void ResetPrefab()
+
+    {
+    SetPrefabIndex();
+    interactable.placementPrefab = Prefabs[prefabIndex];
+    }
         
     }
 }
